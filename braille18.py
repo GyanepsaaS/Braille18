053546531391dd88cef3f1503cda8245ffa6a841
Gyanepsaa Singh <64652949+GyanepsaaS@users.noreply.github.com>
2024-01-19 19:01:16 -0500

    Add main braille18 file

---
+++ braille18.py
@@ -0,0 +1,41 @@
+#buttons:
+#   automatic
+#   rate
+#   capture
+
+# overall text to braille conversion.
+# input is text in the form of a string
+def text_to_braille(text):
+    words = text.split()
+    max_len = get_max_len() #number of braille blocks
+    automatic = get_automatic() #true if in automatic mode
+    update_rate = get_update_rate() #update rate if any
+    
+    i=0
+    while i < len(words):
+        curr_word = words[i]
+        if len(curr_word)>max_len:
+            disp_word = curr_word[:max_len]
+            remaining = curr_word[max_len:]
+            words.pop(i)
+            words.insert(i, remaining)
+        else:
+            disp_word = curr_word
+            i+=1
+        braille_display_word(disp_word)
+        # either automatically shift after specified update rate
+        # or wait for button press to shift
+        if automatic:
+            pass
+        else:
+            pass
+
+# takes in a single word
+# displays it by repeatedly calling 
+# braille_character on a vector of 1s and 0s representing the braille version
+# of the current character
+def braille_display_word(word):
+    pass
+
+def braille_character(bchar):
+    pass
